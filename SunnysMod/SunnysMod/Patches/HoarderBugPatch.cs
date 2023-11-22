065778da9180bca369f5be7da3f9d89fe5efacd2
Sunny <outofinkinc@gmail.com>
2023-11-22 16:58:10 -0500

    Removed LC_API dependency

--- SunnysMod/SunnysMod/Patches/HoarderBugPatch.cs
+++ SunnysMod/SunnysMod/Patches/HoarderBugPatch.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using LC_API.BundleAPI;
+//using LC_API.BundleAPI;
 
 namespace YippeeMod.Patches
 {
@@ -16,10 +16,14 @@ namespace YippeeMod.Patches
         [HarmonyPostfix]
         public static void hoarderBugAudioPatch(ref AudioClip[] ___chitterSFX)
         {
-            AudioClip newChitterSFX = BundleLoader.GetLoadedAsset<AudioClip>("assets/yippee-tbh.mp3");
-            AudioClip[] array = new AudioClip[1];
-            array[0] = newChitterSFX;
-            ___chitterSFX = array;
+
+            //AudioClip newChitterSFX = BundleLoader.GetLoadedAsset<AudioClip>("assets/yippee-tbh.mp3");
+
+            AudioClip[] newChitterSFX = YippeeModBase.newSFX;
+            //AudioClip[] array = new AudioClip[1];
+            //array[0] = newChitterSFX;
+            ___chitterSFX = newChitterSFX;
+            
         }
     }
 }
--- SunnysMod/SunnysMod/Plugin.cs
+++ SunnysMod/SunnysMod/Plugin.cs
@@ -15,7 +15,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
-using LC_API;
 
 namespace YippeeMod
 {
@@ -43,23 +42,29 @@ namespace YippeeMod
             }
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
-            mls.LogInfo("Yippee Mod is awake");
+            mls.LogInfo("Yippee Mod is loading.");
 
             //videoPath = ((BaseUnityPlugin)Plugin.instance).Config.Bind<string>("CustomTelevisionVideo", "CustomVideoPath", "FlipMods-CustomTelevisionVideo/television_video.mp4", "Absolute or local video path. Use forward slashes in your path. Local paths are local to the BepInEx/plugins folder, and should not begin with a slash.");
 
             //string path = AppDomain.CurrentDomain.BaseDirectory;
             //mls.LogInfo(path);
+            string path = Paths.BepInExRootPath + "\\Bundles\\yippeesound";
+            mls.LogInfo(path);
 
-            AssetBundle val = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "yippeesound"));
+            
+            AssetBundle val = AssetBundle.LoadFromFile(path);
             if (val == null)
             {
-                mls.LogError((object)"Failed to load audio assets!");
+                mls.LogError("Failed to load audio assets!");
                 return;
             }
-            newSFX = val.LoadAllAssets<AudioClip>();
+            //mls.LogInfo("Bundle loaded");
+            //newSFX = val.LoadAllAssets<AudioClip>();
+            newSFX = val.LoadAssetWithSubAssets<AudioClip>("assets/yippee-tbh.mp3");
+            //mls.LogInfo("Audio CLip loaded");
 
             //harmony.PatchAll();
-            //harmony.PatchAll(typeof(HoarderBugPatch));
+            harmony.PatchAll(typeof(HoarderBugPatch));
 
             mls.LogInfo("Yippee Mod is loaded. Yippee!!!");
         }
